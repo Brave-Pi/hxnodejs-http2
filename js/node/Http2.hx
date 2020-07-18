c64ebf25dacd6612a68a23ef80394b58594c27e2
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2020-07-18 09:17:04 -0500

    Whoops.

--- js/node/Http2.hx
+++ js/node/Http2.hx
@@ -7,7 +7,7 @@ import js.node.http2.Session;
 @:jsRequire('http2')
 typedef Http2 = js.node.http2.Constants;
 
-typedef Headers = haxe.DynamicAccess<String>;
+typedef Headers = haxe.DynamicAccess<Array<String>>;
 typedef Http1IncomingHttpHeaders = Headers;
 typedef OutgoingHttpHeaders = Headers;
 typedef IncomingHttpHeaders = Headers;
